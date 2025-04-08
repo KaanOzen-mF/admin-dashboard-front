2baa2c313e0593016a7b2793cf726255d2e09bfa
KaanOzen-mF <kaanozn98@gmail.com>
2025-04-08 21:07:40 +0300

    Wrap "Admin Page" in a Link component for improved navigation

--- components/navigation/navbar/index.tsx
+++ components/navigation/navbar/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ThemeSwitcher from "./ThemeSwitcher";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between items-center p-4  bg-green-100">
-      <h1>Admin Page</h1>
+      <h1>
+        <Link href="/">Admin Page</Link>
+      </h1>
       <ThemeSwitcher />
     </nav>
   );
