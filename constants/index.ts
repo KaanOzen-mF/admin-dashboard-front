4643507bcfad7a849734195b5a8e9c8078f138a0
KaanOzen-mF <kaanozn98@gmail.com>
2025-04-08 21:08:10 +0300

    Add sidebarLinks constant for navigation structure

---
+++ constants/index.ts
@@ -0,0 +1,26 @@
+export const sidebarLinks = [
+  {
+    // Icon for Home page
+    //imgURL: "/icons/home.svg",
+    // Route for Home page
+    route: "/",
+    // Display label for Home page
+    label: "Home",
+  },
+  {
+    // Icon for Community section
+    //imgURL: "/icons/users.svg",
+    // Route for Community section
+    route: "/portfolio",
+    // Display label for Community section
+    label: "Portfolio",
+  },
+  {
+    // Icon for Community section
+    //imgURL: "/icons/users.svg",
+    // Route for Community section
+    route: "/todo",
+    // Display label for Community section
+    label: "Todo",
+  },
+];
